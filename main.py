99c36add3acf3949e997d0041438ff4b77e06f7b
Ishuman Agrawal <mann.agrawal17@gmail.com>
2024-12-03 16:18:21 +0530

    Add data validation stage and configuration management
    - Introduced data validation configuration in config.yaml
    - Implemented DataValidation class for file existence checks
    - Created DataValdationTrainingPipeline for managing the validation process
    - Added data validation logic in a new Jupyter notebook and Python module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion  import DataIngestionTrainingPipeline
-
+from textSummarizer.pipeline.stage_02_data_validation import  DataValdationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -9,6 +9,20 @@ STAGE_NAME = "Date Ingestion stage"
 try:
     logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME = "Date Validation stage"
+
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+    data_ingestion = DataValdationTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<")
 except Exception as e:
