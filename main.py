98606bcd56368211bbc4ef697c15476bff6228e0
ishumann <mann.agrawal17@gmail.com>
2024-12-11 17:58:05 +0530

    Refactor model evaluation pipeline and configuration
    - Update .gitignore to include tokenizer
    - Add VSCode settings for spell checking
    - Modify config.yaml for consistent tokenizer path
    - Remove obsolete model training pipeline file
    - Introduce new model evaluation pipeline
    - Enhance ConfigurationManager to retrieve model evaluation config

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.stage_01_data_ingestion  import DataIngestionTraini
 from textSummarizer.pipeline.stage_02_data_validation import  DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import  DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_pipeline import ModelTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation_pipeline import ModelEvaluationPipeline
 from textSummarizer.logging import logger
 
 # STAGE_NAME = "Date Ingestion"
@@ -43,12 +44,24 @@ from textSummarizer.logging import logger
 #     raise e
 
 
-STAGE_NAME = "Model Training"
+# STAGE_NAME = "Model Training"
+
+# try:
+#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<<<")
+#     model_training = ModelTrainingPipeline()
+#     model_training.main()
+#     logger.info(f">>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<<<<")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+
+STAGE_NAME = "Model Evaluation"
 
 try:
     logger.info(f">>>>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<<<")
-    model_training = ModelTrainingPipeline()
-    model_training.main()
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
     logger.info(f">>>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/components/model_evaluation.py
+++ src/textSummarizer/components/model_evaluation.py
@@ -1,77 +1,75 @@
-from transformers import DataCollatorForSeq2Seq, TrainingArguments, Trainer
+from pathlib import Path
+import torch
+import pandas as pd
+from tqdm import tqdm
+import evaluate
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from datasets import load_dataset, load_from_disk
-import torch
-from textSummarizer.entity import ModelTrainerConfig
-
+from textSummarizer.entity import ModelEvaluationConfig
 from dataclasses import dataclass
-from pathlib import Path
 
 
-class ModelTrainer:
-    def __init__(self, config: ModelTrainerConfig):
+class ModelEvaluation:
+    def __init__(self, config: ModelEvaluationConfig):
         self.config = config
 
-    def train(self):
-        """
-        Trains the sequence-to-sequence model using the specified configuration.
-        This method sets up the device (CPU or GPU), loads the tokenizer and model,
-        prepares the data collator, loads the dataset, and configures the training
-        arguments before starting the training process.
-        Attributes:
-            device (str): The device to use for training ('cuda' if GPU is available, otherwise 'cpu').
-            tokenizer (AutoTokenizer): The tokenizer loaded from the specified model checkpoint.
-            model_pegasus (AutoModelForSeq2SeqLM): The sequence-to-sequence model loaded from the specified model checkpoint.
-            seq2seq_data_collator (DataCollatorForSeq2Seq): The data collator for sequence-to-sequence tasks.
-            dataset_samsum_pt (Dataset): The dataset loaded from the specified data path.
-            trainer_args (TrainingArguments): The training arguments configured with the specified parameters.
-        Returns:
-            None
-        """
+    def generate_batch_sized_chunks(self, list_of_elements, batch_size):
+        """split the dataset into smaller batches that we can process simultaneously
+        Yield successive batch-sized chunks from list_of_elements."""
+        for i in range(0, len(list_of_elements), batch_size):
+            yield list_of_elements[i: i + batch_size]
+
+    def calculate_metric_on_test_ds(self, dataset, metric, model, tokenizer,
+                                    batch_size=16, device="cuda" if torch.cuda.is_available() else "cpu",
+                                    column_text="article",
+                                    column_summary="highlights"):
+        article_batches = list(self.generate_batch_sized_chunks(
+            dataset[column_text], batch_size))
+        target_batches = list(self.generate_batch_sized_chunks(
+            dataset[column_summary], batch_size))
+
+        for article_batch, target_batch in tqdm(
+                zip(article_batches, target_batches), total=len(article_batches)):
+
+            inputs = tokenizer(article_batch, max_length=1024,  truncation=True,
+                               padding="max_length", return_tensors="pt")
+
+            summaries = model.generate(input_ids=inputs["input_ids"].to(device),
+                                       attention_mask=inputs["attention_mask"].to(
+                                           device),
+                                       length_penalty=0.8, num_beams=8, max_length=128)
+            ''' parameter for length penalty ensures that the model does not generate sequences that are too long. '''
+
+            # Finally, we decode the generated texts,
+            # replace the  token, and add the decoded texts with the references to the metric.
+            decoded_summaries = [tokenizer.decode(s, skip_special_tokens=True,
+                                                  clean_up_tokenization_spaces=True)
+                                 for s in summaries]
+
+            decoded_summaries = [d.replace("", " ") for d in decoded_summaries]
+
+            metric.add_batch(predictions=decoded_summaries,
+                             references=target_batch)
+
+        #  Finally compute and return the ROUGE scores.
+        score = metric.compute()
+        return score
+
+    def evaluate(self):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
         model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(
-            self.config.model_ckpt).to(device)
-        seq2seq_data_collator = DataCollatorForSeq2Seq(
-            tokenizer, model=model_pegasus)
+            self.config.model_path).to(device)
+
+        # loading data
         dataset_samsum_pt = load_from_disk(self.config.data_path)
 
-        # trainer_args = TrainingArguments(
-        #     output_dir="pegasus-samsum",
-        #     num_train_epochs=1,
-        #     warmup_steps=500,
-        #     per_device_train_batch_size=1,
-        #     per_device_eval_batch_size=1,
-        #     weight_decay=0.01,
-        #     logging_steps=10,
-        #     evaluation_strategy='steps',
-        #     eval_steps=500,
-        #     save_steps=1e6,
-        #     gradient_accumulation_steps=16)
+        rouge_names = ["rouge1", "rouge2", "rougeL", "rougeLsum"]
+        rouge_metric = evaluate.load("rouge")
 
-        trainer_args = TrainingArguments(
-                output_dir=self.config.root_dir,
-                num_train_epochs=self.config.num_train_epochs,
-                warmup_steps=self.config.warmup_steps,
-                per_device_train_batch_size=self.config.per_device_train_batch_size,
-                per_device_eval_batch_size=self.config.per_device_train_batch_size,
-                weight_decay=self.config.weight_decay,
-                logging_steps=self.config.logging_steps,
-                evaluation_strategy=self.config.evaluation_strategy,
-                eval_steps=self.config.eval_steps,
-                save_steps=1e6,
-                gradient_accumulation_steps=self.config.gradient_accumulation_steps)
+        score = self.calculate_metric_on_test_ds(
+            dataset_samsum_pt['test'][0:10], rouge_metric, model_pegasus, tokenizer, batch_size=2, column_text='dialogue', column_summary='summary'
+        )
 
-        trainer = Trainer(model=model_pegasus,
-                          args=trainer_args,
-                          tokenizer=tokenizer, data_collator=seq2seq_data_collator,
-                          train_dataset=dataset_samsum_pt["test"],
-                          # use train when training, here testing just code testing purpose
-                          eval_dataset=dataset_samsum_pt['validation'])
-
-        trainer.train()
-
-        model_pegasus.save_pretrainer(os.path.join(
-            self.config.root_dir, "pegasus-samsum-model"))
-        tokenizer.save_pretrainer(os.path.join(
-            self.config.root_dir, 'tokenizer'))
+        df = pd.DataFrame(score, index=['pegasus'])
+        df.to_csv(self.config.metric_file_name, index=False)
